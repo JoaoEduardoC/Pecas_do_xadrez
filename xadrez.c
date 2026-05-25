44015bf4cc10639d0eebc7f2ac5d1fb60e7e8386
JoaoEduardoC <202601803417@alunos.unifavip.edu.br>
2026-05-25 18:18:50 +0000

    Adicionar cavalo

--- xadrez.c
+++ xadrez.c
@@ -5,16 +5,19 @@ int main()
     // Torre move 5 casas á direita:
     for (int i = 1; i <= 5; i++)
     {
-        printf("Torre move %d casa(s) á direita\n", i);
+        printf("Direita\n", i);
     }
+    printf("\n");
 
     // Bispo move 5 casas á direita e á cima:
     int bispo = 0;
     do
     {   
         bispo++;
-        printf("bispo move %d casa(s) á Cima e á Direita\n", bispo);
+        printf("Direita\n", bispo);
+        printf("Cima\n", bispo);
     } while (bispo <= 4);
+    printf("\n");
 
     // Rainha move 8 casas á esquerda:
     int rainha = 0;
@@ -22,7 +25,21 @@ int main()
     while (rainha < 8)
     {   
         rainha++;
-        printf("Rainha move %d casa(s) á esquerda\n", rainha);
+        printf("Esquerda\n", rainha);
+    }
+    printf("\n");
+
+    // Cavalo move duas casas para baixo e uma a esquerda:
+    int cavaloBaixo = 1;
+    for (int i = 1; i < 2; i++)
+    {
+        while (cavaloBaixo <= 2)
+        {
+            cavaloBaixo++;
+            printf("Baixo\n");
+        }
+        i++;
+        printf("Esquerda\n");
     }
     
 
