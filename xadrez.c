6e6443f9d6ca4f85979097dc90ec822bb6e03352
JoaoEduardoC <202601803417@alunos.unifavip.edu.br>
2026-05-25 19:14:27 +0000

    Adicionar recursividade

--- xadrez.c
+++ xadrez.c
@@ -1,45 +1,67 @@
 #include <stdio.h>
 
+void torre(int c) {
+    if (c > 0) {
+        torre(c - 1);
+        printf("Direita\n");
+    }
+
+    // for (int i = 1; i <= 5; i++)
+    // {
+    //     printf("Direita\n", i);
+    // }
+    // printf("\n");
+}
+
+void bispo(int c) {
+    do
+    {   
+        c--;
+        printf("Direita\n");
+        printf("Cima\n");
+    } while (c > 0);
+    printf("\n");
+}
+
+void rainha(int c) {
+    if (c > 0)
+    {
+        rainha(c - 1);
+        printf("Esquerda\n");
+    }
+    
+    // int rainha = 0;
+
+    // while (rainha < 8)
+    // {   
+    //     rainha++;
+    //     printf("Esquerda\n", rainha);
+    // }
+    // printf("\n");
+}
+
 int main()
 {
     // Torre move 5 casas á direita:
-    for (int i = 1; i <= 5; i++)
-    {
-        printf("Direita\n", i);
-    }
+    torre(5);
     printf("\n");
-
     // Bispo move 5 casas á direita e á cima:
-    int bispo = 0;
-    do
-    {   
-        bispo++;
-        printf("Direita\n", bispo);
-        printf("Cima\n", bispo);
-    } while (bispo <= 4);
+    bispo(5);
     printf("\n");
-
     // Rainha move 8 casas á esquerda:
-    int rainha = 0;
-
-    while (rainha < 8)
-    {   
-        rainha++;
-        printf("Esquerda\n", rainha);
-    }
+    rainha(8);
     printf("\n");
-
-    // Cavalo move duas casas para baixo e uma a esquerda:
+    // Cavalo move duas casas para cima e uma a direita:
     int cavaloBaixo = 1;
     for (int i = 1; i < 2; i++)
     {
         while (cavaloBaixo <= 2)
         {
             cavaloBaixo++;
-            printf("Baixo\n");
+            printf("Cima\n");
         }
         i++;
-        printf("Esquerda\n");
+        printf("Direita\n");
     }
     
 
